25756c5f0f2780ea689bc656b861a7fbf0d91e8f
YoYag <la2043432@gmail.com>
2022-05-03 22:29:03 +0900

    User 엔티티 추가

---
+++ src/main/java/com/yyk/test/user/domain/User.java
@@ -0,0 +1,14 @@
+package com.yyk.test.user.domain;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    public long id;
+    public LocalDateTime regDate;
+    public LocalDateTime updateDate;
+    public String email;
+    public String password;
+    public String name;
+}
